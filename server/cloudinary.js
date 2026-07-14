33f9cc5888951fdb476a462ad36a1740a1123091
Nikhil Kumar Singh <nikhilkumar.singh@infinitylearn.com>
2026-07-14 11:27:44 +0530

    Initial commit

---
+++ server/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+    cloud_name: 'aecckjzm', 
+    api_key: '577686911182149', 
+    api_secret: 'n4J6yTjfKYc6kwbLf5mLSrIRV7w'
+  });
+
+  module.exports = cloudinary;
